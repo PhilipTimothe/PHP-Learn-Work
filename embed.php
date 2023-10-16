dea35059be4beedd18f0cc371f7030e449c83a39
Philippe Timothe <PhilippeTimothe@gmail.com>
2023-10-16 12:51:59 -0400

    dynamic data

--- embed.php
+++ embed.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
 <?php 
+
+$title = "Philp Timothe"
+
+?>
+
+<h1><?php echo $title; ?></h1>
+
+
+</body>
+</html>
