651268628718b0d9893c526dbc1e05a920be2c22
Philippe Timothe <PhilippeTimothe@gmail.com>
2023-10-17 16:24:35 -0400

    form exercise

--- Exercise_Files/_blank.php
+++ Exercise_Files/_blank.php
@@ -1,3 +1,11 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +13,17 @@
     <title>Document</title>
 </head>
 <body>
+
+<form action="form.php" method="post">
+    <input type="text" name="name" placeholder="Enter Name">
+    <input type="submit" name="submit" value="Submit">
+</form>
+
+<?php
+// define a constant as such
+// define("NAME", "Philip");
+// const FIRST_NAME = 'Philip';
+?>
 
 </body>
 </html>
